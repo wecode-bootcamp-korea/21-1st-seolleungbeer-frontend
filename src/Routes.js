bf8a4488df4caa5789c14aac72d8f0b49bbf7423
git-k <kgunu93@gmail.com>
2021-06-07 16:21:56 +0900

    Add : common.scss 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './Style/reset.scss';
+import './Style/common.scss';
 
 class Routes extends React.Component {
   render() {
