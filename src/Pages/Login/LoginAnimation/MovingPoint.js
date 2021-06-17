a911678bb9a7ef013721a75929ad87a9ba93cc69
백진수 <bjinsu93@gmail.com>
2021-06-17 20:00:43 +0900

    Refactor: 불필요한 코드 삭제

--- src/Pages/Login/LoginAnimation/MovingPoint.js
+++ src/Pages/Login/LoginAnimation/MovingPoint.js
@@ -9,16 +9,5 @@ export default class MovingPoint {
   update(canvas, ctx) {
     this.radian += 0.05;
     this.y += Math.sin(this.radian) * 0.7;
-    // this.draw(canvas, ctx);
   }
-
-  // draw(canvas, ctx) {
-  //   // console.log(canvas, ctx);
-  //   // ctx.fillStyle = 'white';
-  //   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //   ctx.fillStyle = 'red';
-  //   ctx.beginPath();
-  //   ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-  //   ctx.fill();
-  // }
 }
